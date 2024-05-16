6a97268f4f657d12ea960dced9c493a7fe7b3aee
mjcarnaje <michaeljamescarnaje1@gmail.com>
2024-05-17 04:59:41 +0800

    refactor: naming variables & functions

--- index.py
+++ index.py
@@ -8,14 +8,13 @@ size = comm.Get_size()
 next_bucket = 1
 
 def save_last_key(key):
-    with open("last_key.txt", "w") as f:
-        f.write(str(key))
-        f.close()
+    with open("last_key.txt", "w") as file:
+        file.write(str(key))
 
 def get_last_key():
     try:
-        with open("last_key.txt", "r") as f:
-            return int(f.read())
+        with open("last_key.txt", "r") as file:
+            return int(file.read())
     except FileNotFoundError:
         return None
 
@@ -26,7 +25,7 @@ def insert_data(value):
     next_bucket = (next_bucket % (size - 1)) + 1
     save_last_key(key)
 
-def r_insert_data(key, value):
+def rebalance_insert_data(key, value):
     global next_bucket
     comm.send({'action': 'insert', 'key': key, 'value': value}, dest=next_bucket)
     next_bucket = (next_bucket % (size - 1)) + 1
@@ -40,7 +39,7 @@ def get_data(key):
     for bucket in range(1, size):
         comm.send({'action': 'get', 'key': key}, dest=bucket)
 
-def set_data(key, value):
+def update_data(key, value):
     for bucket in range(1, size):
         comm.send({'action': 'update', 'key': key, 'value': value}, dest=bucket)
 
@@ -50,16 +49,16 @@ def find_data(key):
 
 def load_last_key():
     try:
-        with open("last_key.txt", "r") as f:
-            return f.read()
+        with open("last_key.txt", "r") as file:
+            return file.read()
     except FileNotFoundError:
         return None
 
 def find_in_bucket(key):
     found = False
 
-    with open(f"bucket_{rank}.txt", "r") as f:
-        for line in f:
+    with open(f"bucket_{rank}.txt", "r") as file:
+        for line in file:
             id, value = line.strip().split(':')
             if key == id:
                 found = True
@@ -70,112 +69,107 @@ def find_in_bucket(key):
 def get_from_bucket(key):
     value = None
 
-    with open(f"bucket_{rank}.txt", "r") as f:
-        for line in f:
+    with open(f"bucket_{rank}.txt", "r") as file:
+        for line in file:
             id, val = line.strip().split(':')
             if key == id:
                 value = val
                 break
-   
+
     return value
 
 def delete_from_bucket(key):
-    with open(f"bucket_{rank}.txt", "r+") as f:
-        lines = f.readlines()
-        f.seek(0)
+    with open(f"bucket_{rank}.txt", "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
         for line in lines:
             id, value = line.strip().split(':')
             if key != id:
-                f.write(line)
-        f.truncate()
-    
+                file.write(line)
+        file.truncate()
 
 def update_in_bucket(key, new_value):
-    with open(f"bucket_{rank}.txt", "r+") as f:
-        lines = f.readlines()
-        f.seek(0)
+    with open(f"bucket_{rank}.txt", "r+") as file:
+        lines = file.readlines()
+        file.seek(0)
         for line in lines:
             id, value = line.strip().split(':')
             if key == id:
-                f.write(f"{id}:{new_value}\n")
+                file.write(f"{id}:{new_value}\n")
             else:
-                f.write(line)
-        f.truncate()
+                file.write(line)
+        file.truncate()
 
-def create_bucket():
+def create_buckets():
     for i in range(1, size):
         if not os.path.exists(f"bucket_{i}.txt"):
             open(f"bucket_{i}.txt", "w").close()
 
-def check_if_balance():
-    list_of_buckets = [f"bucket_{i}.txt" for i in range(1, size)]
-    map_lines = {}
-    all_lines = 0
+def is_balanced():
+    bucket_files = [f"bucket_{i}.txt" for i in range(1, size)]
+    bucket_line_counts = {}
+    total_lines = 0
 
-    for file in list_of_buckets:
+    for bucket_file in bucket_files:
         try:
-            with open(file) as f:
-                file_lines = len(f.readlines())
-                f.close()
-            map_lines[file] = file_lines
-            all_lines += file_lines
+            with open(bucket_file) as file:
+                line_count = len(file.readlines())
+            bucket_line_counts[bucket_file] = line_count
+            total_lines += line_count
         except FileNotFoundError:
-            map_lines[file] = 0
+            bucket_line_counts[bucket_file] = 0
 
-    print(f"All lines: {all_lines}")
+    print(f"Total lines: {total_lines}")
 
-    if all_lines < size:
-        return True, map_lines
+    if total_lines < size:
+        return True, bucket_line_counts
 
-    average_lines = all_lines / len(map_lines)
+    average_lines = total_lines / len(bucket_line_counts)
 
-    for file, lines in map_lines.items():
-        if lines > 1.3 * average_lines or lines < 0.7 * average_lines:
-            return False, map_lines
+    for bucket_file, line_count in bucket_line_counts.items():
+        if line_count > 1.3 * average_lines or line_count < 0.7 * average_lines:
+            return False, bucket_line_counts
 
-    return True, map_lines
+    return True, bucket_line_counts
 
-def rebalance_data(map_lines):
+def rebalance_buckets(bucket_line_counts):
     all_data = []
 
-    for file in map_lines.keys():
+    for bucket_file in bucket_line_counts.keys():
         try:
-            with open(file, "r") as f:
-                all_data.extend(f.readlines())
+            with open(bucket_file, "r") as file:
+                all_data.extend(file.readlines())
         except FileNotFoundError:
             pass
 
     if len(all_data) == 0:
         return
 
-    # delete all files
-    for file in map_lines.keys():
-        open(file, "w").close()
+    for bucket_file in bucket_line_counts.keys():
+        open(bucket_file, "w").close()
 
-    print(f"Data before rebalance: ")
+    print("Data before rebalance:")
 
-    for key, value in map_lines.items():
-        print(f"Bucket {key} has {value} lines.")
+    for bucket_file, line_count in bucket_line_counts.items():
+        print(f"Bucket {bucket_file} has {line_count} lines.")
 
-    # redistribute data
     for data in all_data:
         key, value = data.strip().split(':')
-        r_insert_data(key, value)
+        rebalance_insert_data(key, value)
 
-def save_in_bucket(key, value):
-    with open(f"bucket_{rank}.txt", "a") as f:
-        f.write(f"{key}:{value}\n")
-        f.close()
+def save_to_bucket(key, value):
+    with open(f"bucket_{rank}.txt", "a") as file:
+        file.write(f"{key}:{value}\n")
 
 if rank == 0:
-    create_bucket()
+    create_buckets()
 
-    for i in range(10000):
+    for _ in range(10000):
         insert_data(str(uuid.uuid4()))
-    
+
     get_data("1")
-    
-    set_data("1", "new_value")
+
+    update_data("1", "new_value")
 
     get_data("1")
 
@@ -185,42 +179,39 @@ if rank == 0:
 
     find_data("2")
 
-    balance, map_lines = check_if_balance()
-    
-    print(f"Is the data balanced? {balance}")
-    
-    while not balance:
-        rebalance_data(map_lines)
-        balance, map_lines = check_if_balance()
-        print(f"Is the data balanced? {balance}")
-    
-    for key, value in map_lines.items():
-        print(f"Bucket {key} has {value} lines.")
-    
+    balanced, bucket_line_counts = is_balanced()
+
+    print(f"Is the data balanced? {balanced}")
+
+    while not balanced:
+        rebalance_buckets(bucket_line_counts)
+        balanced, bucket_line_counts = is_balanced()
+        print(f"Is the data balanced? {balanced}")
+
+    for bucket_file, line_count in bucket_line_counts.items():
+        print(f"Bucket {bucket_file} has {line_count} lines.")
+
     for i in range(1, size):
         comm.send({'action': 'stop'}, dest=i)
 else:
     while True:
-        data = comm.recv(source=0)
-        
-        if data['action'] == 'stop':
+        message = comm.recv(source=0)
+
+        if message['action'] == 'stop':
             break
-        elif data['action'] == 'insert':
-            save_in_bucket(data['key'], data['value'])
-        elif data['action'] == 'delete':
-            delete_from_bucket(data['key'])
-        elif data['action'] == 'update':
-            update_in_bucket(data['key'], data['value'])
-        elif data['action'] == 'get':
-            res = get_from_bucket(data['key'])
-
-            if res:
-                print(f"Data for key {data['key']} is: {res}")
-
-        elif data['action'] == 'find':
-            found = find_in_bucket(data['key'])
-
+        elif message['action'] == 'insert':
+            save_to_bucket(message['key'], message['value'])
+        elif message['action'] == 'delete':
+            delete_from_bucket(message['key'])
+        elif message['action'] == 'update':
+            update_in_bucket(message['key'], message['value'])
+        elif message['action'] == 'get':
+            result = get_from_bucket(message['key'])
+            if result:
+                print(f"Data for key {message['key']} is: {result}")
+        elif message['action'] == 'find':
+            found = find_in_bucket(message['key'])
             if found:
-                print(f"Key {data['key']} found in bucket {rank}: {found}")
-
+                print(f"Key {message['key']} found in bucket {rank}")
+                
 comm.Barrier()
